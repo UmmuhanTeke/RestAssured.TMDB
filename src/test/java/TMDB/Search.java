acd535491430287aa35d3499a43010717ee29086
kayyamervee <kayaamervee01@gmail.com>
2024-11-26 20:41:13 +0300

    movies and search has been completed

--- src/test/java/TMDB/Search.java
+++ src/test/java/TMDB/Search.java
@@ -1,7 +1,57 @@
 package TMDB;
+
+import TMDB.Utility.TMDB_ParentPage;
+import org.testng.annotations.Test;
+
 import static io.restassured.RestAssured.*;
-import static org.hamcrest.Matchers.*;
 
+public class Search extends TMDB_ParentPage {
 
-public class Search {
+    @Test
+    public void movie() {
+
+        given()
+                .spec(reqSpec)
+                .when()
+                .get("search/movie?Query=Hot Frosty")
+                .then()
+                .log().body()
+                .statusCode(200);
+    }
+
+    @Test
+    public void TV() {
+
+        given()
+                .spec(reqSpec)
+                .when()
+                .get("search/tv?Query=The Rookie")
+                .then()
+                .log().body()
+                .statusCode(200);
+    }
+
+    @Test
+    public void person() {
+
+        given()
+                .spec(reqSpec)
+                .when()
+                .get("search/person?Query=Scarlett Johansson")
+                .then()
+                .log().body()
+                .statusCode(200);
+    }
+
+    @Test
+    public void keyword() {
+
+        given()
+                .spec(reqSpec)
+                .when()
+                .get("search/keyword?Query=Movie")
+                .then()
+                .log().body()
+                .statusCode(200);
+    }
 }
